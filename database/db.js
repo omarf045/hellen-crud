41b9789675e118eca5378df33fc9b202b16192bf
omarf045 <pulido.morales.omar@gmail.com>
2022-10-14 17:04:51 -0500

    first commit 3

--- database/db.js
+++ database/db.js
@@ -1,21 +1,22 @@
-const mysql =require('mysql');
+const mysql = require("mysql");
 
+import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } from "../config.js";
 
 const conexion = mysql.createConnection({
-    host: 'containers-us-west-56.railway.app',
-    user: 'root',
-    password: 'w9KWTEd2VOYwJrytmom9',
-    database: 'railway',
-    PORT:"5820",
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  PORT: DB_PORT,
 });
 
-conexion.connect((error)=>{
-    if(error){
-        console.error('El error de conexion es:'+error);
-        return
-    }
+conexion.connect((error) => {
+  if (error) {
+    console.error("El error de conexion es:" + error);
+    return;
+  }
 
-    console.log('!Conectado a la BD MYSQL¡');
-})
+  console.log("!Conectado a la BD MYSQL¡");
+});
 
 module.exports = conexion;
